b4df17b911860418f0f76112cd5e01a2a7448791
Scott Gledhill <scottgledhill@hotmail.co.uk>
2018-11-06 18:21:18 +0000

    action types defin ed as constant;

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker'
+import * as serviceWorker from './serviceWorker';
+import { Provider } from 'react-redux'
+import { createStore } from 'redux'
+// import chat from './reducers'
 
-const store = createStore(chat)
+const store = createStore()
 
 ReactDOM.render(
   <Provider store={store}>
@@ -12,4 +15,7 @@ ReactDOM.render(
   </Provider>,
   document.getElementById('root')
 )
-registerServiceWorker()
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: http://bit.ly/CRA-PWA
+serviceWorker.unregister();
